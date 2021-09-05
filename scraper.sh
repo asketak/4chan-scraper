4f81aac5c2d732f319636ce9557f0d292cf98daf
Tomáš Šíma <simatomas2000@gmail.com>
2021-09-05 15:27:08 +0200

    a

--- scraper.sh
+++ scraper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://boards.4channel.org/biz/catalog | tr ":" "\n" | egrep -wE '},"[0-9]+"' | rev | cut -d "," -f1 | rev | tr -d "\"" | xargs -I % sh -c 'echo executing thread %; nohup thread-archiver --nocss --nojs --thumbsonly --nothumbs --runonce https://boards.4channel.org/biz/thread/% --delay 5 &'
+wget -q -O - https://boards.4channel.org/biz/catalog | tr ":" "\n" | egrep -wE '},"[0-9]+"' | rev | cut -d "," -f1 | rev | tr -d "\"" | xargs -I % sh -c 'echo executing thread %; thread-archiver --nocss --nojs --thumbsonly --nothumbs --runonce https://boards.4channel.org/biz/thread/% --delay 5 '
 
